0bda640356915320c69572e94d01362d363fc8e5
Lleuad <eriknonhebel@hetnet.nl>
2018-07-23 19:49:41 +0200

    added cortrol constructs (Part V)

--- SRC/control.c
+++ SRC/control.c
@@ -1,21 +1,203 @@
 #include "cradle.h"
 #include "control.h"
 
+void Condition(void);  //Parse and Translate a Boolean Condition
+void Expr(void); //Parse and Translate an Expression
+
 //Recognize and Translate an "Other"
 void Other(){
     Emit("%s\n", GetName());
 }
 
+//Parse and Translate a Boolean Condition
+//FIXME dummy
+void Condition(){
+    Emit("<condition>\n");
+}
+
+//Parse and Translate a Expression
+//FIXME dummy
+void Expr(){
+    Emit("<expr>\n");
+}
+
+//Recognize and Translate an IF Construct
+//<IF-statement> ::= IF <condition> <block> ["ELSE" <block>] "ENDIF"
+void DoIf(char *L){
+    char* L1;
+    char* L2;
+    Match('i');
+    Condition();
+    L1 = NewLabel();
+    L2 = NULL;
+    Emit("BEQ %s\n", L1);
+    Block(L);
+    if (Look == 'l') {
+        Match('l');
+        L2 = NewLabel();
+        Emit("BRA %s\n", L2);
+        PostLabel(L1);
+        Block(L);
+    }
+    Match('e');
+    PostLabel(L2 ? L2 : L1);
+    free(L1);
+    free(L2);
+}
+
+//Recognize and Translate a WHILE Construct
+//<WHILE-statement> ::= "WHILE" <condition> <block> "ENDWHILE"
+void DoWhile(){
+    char *L1, *L2;
+    Match('w');
+    L1 = NewLabel();
+    L2 = NewLabel();
+    PostLabel(L1);
+    Condition();
+    Emit("BEQ %s\n", L2);
+    Block(L2);
+    Match('e');
+    Emit("BRA %s\n", L1);
+    PostLabel(L2);
+    free(L1);
+    free(L2);
+}
+
+//Recognize and Translate a LOOP Construct
+//<LOOP-statement> ::= "LOOP" <block> "ENDLOOP"
+void DoLoop(){
+    char *L1, *L2;
+    Match('p');
+    L1 = NewLabel();
+    L2 = NewLabel();
+    PostLabel(L1);
+    Block(L2);
+    Match('e');
+    Emit("BRA %s\n", L1);
+    PostLabel(L2);
+
+    free(L1);
+    free(L2);
+}
+
+//Recognize and Translate a REPEAT Construct
+//<REPEAT-statement> ::= "REPEAT" <block> "UNTIL" <condition>
+void DoRepeat(){
+    char *L1, *L2;
+    Match('r');
+    L1 = NewLabel();
+    L2 = NewLabel();
+    PostLabel(L1);
+    Block(L2);
+    Match('u');
+    Condition();
+    Emit("BEQ %s\n", L1);
+    PostLabel(L2);
+
+    free(L1);
+    free(L2);
+}
+
+//Recognize and Translate a FOR Construct
+//<FOR-statement> ::= "FOR" <expr1> <expr2> <block> "ENDFOR"
+void DoFor(){
+    char *L1, *L2, *Name;
+    Match('f');
+    L1 = NewLabel();
+    L2 = NewLabel();
+    Name = GetName();
+    Match('=');
+    Expr();
+    Emit("SUBQ #1, D0\n");
+    Emit("LEA %s(PC), A0\n", Name);
+    Emit("MOVE D0, (A0)\n");
+    Expr();
+    Emit("MOVE D0, -(SP)\n");
+    PostLabel(L1);
+    Emit("LEA %s(PC), A0\n", Name);
+    Emit("MOVE (A0), D0\n");
+    Emit("ADDQ #1, D0\n");
+    Emit("MOVE D0, (A0)\n");
+    Emit("CMP (SP), D0\n");
+    Emit("BGT %s\n", L2);
+    Block(L2);
+    Match('e');
+    Emit("BRA %s\n", L1);
+    PostLabel(L2);
+    Emit("ADDQ #2, SP\n");
+
+    free(L1);
+    free(L2);
+    free(Name);
+}
+
+//Recognize and Translate a DO Construct
+//<DO-statement> ::= "DO" <expr> <block> "ENDDO"
+void DoDo(){
+    char *L1, *L2;
+    Match('d');
+    L1 = NewLabel();
+    L2 = NewLabel();
+    Expr();
+    Emit("SUBQ #1, D0\n");
+    PostLabel(L1);
+    Emit("MOVE D0, -(SP)\n");
+    Block(L2);
+    Emit("MOVE (SP)+, D0\n");
+    Emit("DBRA D0, %s\n", L1);
+    Emit("SUBQ #2, SP\n");
+    PostLabel(L2);
+    Emit("ADDQ #2, SP\n");
+
+    free(L1);
+    free(L2);
+}
+
+//Recognize and Translate a BREAK Statement
+//<BREAK-statement> ::= "BREAK"
+void DoBreak(char *L){
+    Match('b');
+    if (L) Emit("BRA %s\n", L);
+    else Abort("Can't break from global scope.\n");
+}
+
 //Recognize and Translate a Statement Block
 //<block> ::= [ <statement> ]*
-void Block(){
-    while (Look != 'e') Other();
+void Block(char *L){
+    while ( !(Look == 'e' || Look == 'l' || Look == 'u') ){
+        switch (Look) {
+            case 'i':
+                DoIf(L);
+                break;
+            case 'w':
+                DoWhile();
+                break;
+            case 'p':
+                DoLoop();
+                break;
+            case 'r':
+                DoRepeat();
+                break;
+            case 'f':
+                DoFor();
+                break;
+            case 'd':
+                DoDo();
+                break;
+            case 'b':
+                DoBreak(L);
+                break;
+            default:
+                Other();
+                break;
+        }
+    }
 }
 
 //Parse and Translate a Program
 //<program> ::= <block> END
 void DoProgram(){
-    Block();
+    Block(NULL);
     if (Look != 'e') Expected("End");
     Emit("END\n");
 }
--- SRC/control.h
+++ SRC/control.h
@@ -1,8 +1,15 @@
 #ifndef CONTROLE_H_
 #define CONTROLE_H_
 
-void Other(void);     //Recognize and Translate an "Other"
-void Block(void);     //Recognize and Translate a Statement Block
-void DoProgram(void); //Parse and Translate a Program
+void Other(void);      //Recognize and Translate an "Other"
+void DoIf(char *);     //Recognize and Translate an IF Construct
+void DoWhile(void);    //Recognize and Translate an WHILE Construct
+void DoLoop(void);     //Recognize and Translate an LOOP Construct
+void DoRepeat(void);   //Recognize and Translate an REPEAT Construct
+void DoFor(void);      //Recognize and Translate an REPEAT Construct
+void DoDo(void);       //Recognize and Translate a DO Construct
+void DoBreak(char *);  //Recognize and Translate a BREAK Statement
+void Block(char *);    //Recognize and Translate a Statement Block
+void DoProgram(void);  //Parse and Translate a Program
 
 #endif//CONTROLE_H_
